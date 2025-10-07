9ec0454addaa1ce669d5460443652cebafae1a4b
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2025-10-07 15:45:14 +0530

    Added Better code using hashmap

---
+++ lesson1/BetterCode/v2/Product.java
@@ -0,0 +1,29 @@
+package lesson1.BetterCode.v2;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Product {
+    private String name;
+    private String desc;
+    private int price;
+    private String brand;
+    private String category;
+    private int discount;
+    private String createdAt;
+    private String updatedAt;
+    private List<String> images;
+
+    Product(HashMap<String, Object> mp) {
+        if(mp.get("price").equals(0)) {
+            // this.price = mp.get("price");
+        }
+        
+    }
+
+    // problem :- What if someone forget to specify the key in the hash map and what if there is a spelling mistakes
+
+    
+    
+}
+
